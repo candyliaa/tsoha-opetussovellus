6e09e6d4f1aae827c99bfdb780f49aadbb464014
astro <astronomy.dev@gmail.com>
2023-11-08 17:16:20 +0200

    functional db connection

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from flask import redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///tsoha"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres@localhost/tsoha"
 db = SQLAlchemy(app)
 
-db.session.execute("CREATE TABLE testi (id SERIAL PRIMARY KEY, content TEXT);")
-db.session.execute("INSERT INTO testi (content) VALUES ('just testing!');")
-db.session.execute("SELECT * FROM testi;")
+with app.app_context():
+    db.session.execute(text("CREATE TABLE IF NOT EXISTS test (id SERIAL PRIMARY KEY, content TEXT);"))
+    db.session.commit()
+    db.session.execute(text("INSERT INTO test (content) VALUES ('just testing!');"))
+    db.session.commit()
 
 @app.route("/")
 def index():
-    result = db.session.execute("SELECT content FROM test")
-    return result
+    result = db.session.execute(text("SELECT * FROM test;"))
+    return result.fetchone()[1]
