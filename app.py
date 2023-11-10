2f131d75dd963fa93cea1756d80512beb11bd294
astro <astronomy.dev@gmail.com>
2023-11-10 14:13:12 +0200

    structured app with new files

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 from flask import Flask
-from flask import redirect, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import text
+from os import getenv
 
 app = Flask(__name__, static_url_path="", static_folder="public", template_folder="templates")
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres@localhost/tsoha"
-db = SQLAlchemy(app)
+app.secret_key = getenv("SECRET_KEY")
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+import routes
--- schema.sql
+++ schema.sql
@@ -2,14 +2,14 @@ CREATE DATABASE tsoha;
 
 \c tsoha
 
-CREATE TABLE student_accounts (id SERIAL PRIMARY KEY, name TEXT, password TEXT)
+CREATE TABLE student_accounts (id SERIAL PRIMARY KEY, name TEXT, password TEXT);
 
-CREATE TABLE teacher_accounts (id SERIAL PRIMARY KEY, name TEXT, password TEXT)
+CREATE TABLE teacher_accounts (id SERIAL PRIMARY KEY, name TEXT, password TEXT);
 
-CREATE TABLE courses (id SERIAL PRIMARY KEY, name TEXT, credits INTEGER)
+CREATE TABLE courses (id SERIAL PRIMARY KEY, name TEXT, credits INTEGER);
 
-CREATE TABLE course_teachers (course_id REFERENCES courses(id), teacher_id REFERENCES teacher_accounts(id))
+CREATE TABLE course_teachers (course_id REFERENCES courses(id), teacher_id REFERENCES teacher_accounts(id));
 
-CREATE TABLE course_participants (course_id REFERENCES courses(id), student_id REFERENCES student_accounts(id))
+CREATE TABLE course_participants (course_id REFERENCES courses(id), student_id REFERENCES student_accounts(id));
 
-CREATE TABLE exercises (id SERIAL PRIMARY KEY, type TEXT)
+CREATE TABLE exercises (id SERIAL PRIMARY KEY, type TEXT);
