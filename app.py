d141251f313ae560fed051e7f02280d255acc050
astro <astronomy.dev@gmail.com>
2023-11-11 14:21:02 +0200

    using env variables in app.py

--- app.py
+++ app.py
@@ -3,5 +3,6 @@ from os import getenv
 
 app = Flask(__name__, static_url_path="", static_folder="public", template_folder="templates")
 app.secret_key = getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 
 import routes
