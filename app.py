b204e75d0f2053e77b4f911404394db384dac3e3
astro <astronomy.dev@gmail.com>
2023-11-11 16:26:35 +0200

    removed unnecessary reference to db in app.py

--- app.py
+++ app.py
@@ -2,6 +2,5 @@ from flask import Flask
 from os import getenv
 
 app = Flask(__name__, static_url_path="", static_folder="public", template_folder="templates")
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 
 import routes
