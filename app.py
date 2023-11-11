3a179b81ba7d7fefe8513a54f1ac887e7e4f28f6
astro <astronomy.dev@gmail.com>
2023-11-11 16:43:26 +0200

    moved app.secret_key to app.py

--- app.py
+++ app.py
@@ -2,5 +2,6 @@ from flask import Flask
 from os import getenv
 
 app = Flask(__name__, static_url_path="", static_folder="public", template_folder="templates")
+app.secret_key = getenv("SECRET_KEY")
 
 import routes
