c7fd38a7c2e5792e835bf52123cdaa9d45476e82
astro <astronomy.dev@gmail.com>
2023-11-11 14:19:29 +0200

    routes for login and account created pages

--- routes.py
+++ routes.py
@@ -4,3 +4,11 @@ from flask import render_template, request, redirect
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login")
+def login():
+    pass
+
+@app.route("/accountcreated", methods=["POST"])
+def accountcreated():
+    return render_template("accountcreated.html", name=request.form["name"])
