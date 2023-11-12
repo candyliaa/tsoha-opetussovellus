3457c6e53e6e9b005e72c8eb48701b304e9d83c4
astro <astronomy.dev@gmail.com>
2023-11-12 15:58:28 +0200

    exercises has course_id column

--- schema.sql
+++ schema.sql
@@ -14,4 +14,4 @@ CREATE TABLE course_teachers (course_id INTEGER REFERENCES courses(id), teacher_
 
 CREATE TABLE course_participants (course_id INTEGER REFERENCES courses(id), student_id INTEGER REFERENCES student_accounts(id));
 
-CREATE TABLE exercises (id SERIAL PRIMARY KEY, type TEXT);
+CREATE TABLE exercises (id SERIAL PRIMARY KEY, type TEXT, course_id INTEGER REFERENCES courses(id));
