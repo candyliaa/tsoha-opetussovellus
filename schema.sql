3d8f6b29ef37669459414f177754bf9b7cfd0ce8
astro <astronomy.dev@gmail.com>
2023-11-12 18:04:48 +0200

    made names unique for student accounts, teacher accounts and courses

--- schema.sql
+++ schema.sql
@@ -4,14 +4,14 @@ CREATE DATABASE tsoha;
 
 \c tsoha
 
-CREATE TABLE student_accounts (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
+CREATE TABLE student_accounts (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT);
 
-CREATE TABLE teacher_accounts (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
+CREATE TABLE teacher_accounts (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT);
 
-CREATE TABLE courses (id SERIAL PRIMARY KEY, name TEXT, credits INTEGER);
+CREATE TABLE courses (id SERIAL PRIMARY KEY, name TEXT UNIQUE, credits INTEGER);
 
 CREATE TABLE course_teachers (course_id INTEGER REFERENCES courses(id), teacher_id INTEGER REFERENCES teacher_accounts(id));
 
 CREATE TABLE course_participants (course_id INTEGER REFERENCES courses(id), student_id INTEGER REFERENCES student_accounts(id));
 
-CREATE TABLE exercises (id SERIAL PRIMARY KEY, type TEXT, course_id INTEGER REFERENCES courses(id));
+CREATE TABLE exercises (id SERIAL PRIMARY KEY, name TEXT, type TEXT, course_id INTEGER REFERENCES courses(id));
