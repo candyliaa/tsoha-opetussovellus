2651c223aca820143b94901ce2e9b2fb748e2e59
astro <astronomy.dev@gmail.com>
2023-11-12 15:58:10 +0200

    sql file for adding test data to the database

---
+++ test_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO student_accounts (username, password) VALUES ('opiskelija1', 'qwerty')
+INSERT INTO student_accounts (username, password) VALUES ('opiskelija2', '12345')
+
+INSERT INTO teacher_accounts (username, password) VALUES ('opettaja1', 'asdfg')
+INSERT INTO teacher_accounts (username, password) VALUES ('opettaja2', '67890')
+
+INSERT INTO courses (name, credits) VALUES ('ohpe', 5)
+INSERT INTO courses (name, credits) VALUES ('ohja', 5)
