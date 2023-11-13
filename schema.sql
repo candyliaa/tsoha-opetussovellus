cc6c73edf8cf275aa1630e9d089ca0b5bb1ef1e2
astro <astronomy.dev@gmail.com>
2023-11-13 14:10:41 +0200

    added ON DELETE CASCADE to course_id in course_teachers and course_participants

--- schema.sql
+++ schema.sql
@@ -10,8 +10,8 @@ CREATE TABLE teacher_accounts (id SERIAL PRIMARY KEY, username TEXT UNIQUE, pass
 
 CREATE TABLE courses (id SERIAL PRIMARY KEY, name TEXT UNIQUE, credits INTEGER);
 
-CREATE TABLE course_teachers (course_id INTEGER REFERENCES courses(id), teacher_id INTEGER REFERENCES teacher_accounts(id));
+CREATE TABLE course_teachers (course_id INTEGER REFERENCES courses(id) ON DELETE CASCADE, teacher_id INTEGER REFERENCES teacher_accounts(id));
 
-CREATE TABLE course_participants (course_id INTEGER REFERENCES courses(id), student_id INTEGER REFERENCES student_accounts(id));
+CREATE TABLE course_participants (course_id INTEGER REFERENCES courses(id) ON DELETE CASCADE, student_id INTEGER REFERENCES student_accounts(id));
 
 CREATE TABLE exercises (id SERIAL PRIMARY KEY, name TEXT, type TEXT, course_id INTEGER REFERENCES courses(id));
