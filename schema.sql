041658aa7b0c53551c397d007671708feca9148f
astro <astronomy.dev@gmail.com>
2023-11-14 15:19:02 +0200

    added exercises column to table courses

--- schema.sql
+++ schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE student_accounts (id SERIAL PRIMARY KEY, username TEXT UNIQUE, pass
 
 CREATE TABLE teacher_accounts (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT);
 
-CREATE TABLE courses (id SERIAL PRIMARY KEY, name TEXT UNIQUE, credits INTEGER);
+CREATE TABLE courses (id SERIAL PRIMARY KEY, name TEXT UNIQUE, credits INTEGER, exercises INTEGER);
 
 CREATE TABLE course_teachers (course_id INTEGER REFERENCES courses(id) ON DELETE CASCADE, teacher_id INTEGER REFERENCES teacher_accounts(id));
 
